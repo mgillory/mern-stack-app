8dd7c3cb0dcbb88cc562eeb21aaf609f73e35aa0
mgillory <mathfreitas15@gmail.com>
2018-09-18 15:21:26 -0300

    post routes

---
+++ validation/post.js
@@ -0,0 +1,17 @@
+const isEmpty = require("./is-empty");
+const Validator = require("validator");
+
+module.exports = validatePostInput = data => {
+  let errors = {};
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 }))
+    errors.text = "Text field must be between 10 and 300 characters";
+
+  if (Validator.isEmpty(data.text)) errors.text = "Text field is required";
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
