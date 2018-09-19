ad572d4dfe6293fb2cb33b30bf0e3f4eb61a20a5
mgillory <mathfreitas15@gmail.com>
2018-09-19 01:41:36 -0300

    added login and register front-end, as well as form validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
