fbab86b7e75c2c07d354815f687d13b37d8e0b2f
mgillory <mathfreitas15@gmail.com>
2018-09-19 14:12:26 -0300

    redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
 import Landing from "./components/layout/Landing";
@@ -11,17 +13,19 @@ import Register from "./components/auth/Register";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <NavBar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
